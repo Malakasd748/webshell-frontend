ffee138e45024bfd007c66abb6d9fbe59451f1d7
wicii <wicii2120@outlook.com>
2025-02-11 20:50:13 +0800

    chore: style init

---
+++ src/styles/index.ts
@@ -0,0 +1,1 @@
+import.meta.glob('./**/*.css', { eager: true })
---
+++ src/theme/naiveThemeOverride.ts
@@ -0,0 +1,38 @@
+import type { GlobalThemeOverrides } from 'naive-ui'
+
+const naiveThemeOverride: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#3f8cff',
+    primaryColorHover: '3f8cff',
+    textColorDisabled: 'rgba(255, 255, 255, 0.35)',
+    borderRadius: '2px',
+  },
+  Tabs: {
+    colorSegment: 'rgba(255, 255, 255, 0.04)',
+    tabTextColorHoverSegment: '#3f8cff',
+    tabTextColorActiveSegment: '#3f8cff',
+    tabColorSegment: 'var(--panel-background-color)',
+    tabBorderRadius: 0,
+    tabPaddingMediumSegment: '8px 8px',
+  },
+  Divider: {
+    color: 'rgba(255, 255, 255, 0.35)',
+  },
+  Switch: {
+    railColorActive: '#3f8cff',
+  },
+  Dropdown: {
+    color: '#434343',
+  },
+  Popover: {
+    color: '#434343',
+  },
+  Popselect: {
+    color: '#434343',
+  },
+  Tooltip: {
+    color: '#434343',
+  },
+}
+
+export default naiveThemeOverride
