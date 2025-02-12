9657057ad20a4b2214605bf455aa605601305382
wicii <wicii2120@outlook.com>
2025-02-12 18:30:25 +0800

    refactor: 逻辑拆分

--- src/stores/webShellResource.ts
+++ src/stores/webShellResource.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { useAsyncState } from '@vueuse/core'
 import { defineStore } from 'pinia'
 
@@ -6,6 +6,7 @@ import { LocalhostResource } from '@/models/resources/localhostResource'
 
 export const useWebShellResourceStore = defineStore('webshell-resource', () => {
   const localhostResource = new LocalhostResource({ id: '123', name: 'localhost', port: 1234 })
+
   const fetchResources = useAsyncState<LocalhostResource[]>(
     async () => {
       await new Promise(resolve => setTimeout(resolve, 1000))
@@ -15,13 +16,22 @@ export const useWebShellResourceStore = defineStore('webshell-resource', () => {
     { immediate: false },
   )
 
-  const selectedResourceId = ref<string>()
-  const activeManager = computed(() => localhostResource.manager)
+  const selectedResource = ref<LocalhostResource>()
+
+  function selectResourceById(id: string) {
+    const resource = fetchResources.state.value.find(r => r.id === id)
+    if (resource) {
+      selectedResource.value = resource
+      return true
+    }
+    return false
+  }
 
   return {
     resources: fetchResources.state,
-    selectedResourceId,
-    activeManager,
+    selectedResource,
+
+    selectResourceById,
     fetchResources: () => fetchResources.execute(),
   }
 })
--- src/stores/webShellTerm.ts
+++ src/stores/webShellTerm.ts
@@ -1,14 +1,16 @@
-import { shallowRef } from 'vue'
+import { shallowRef, watchEffect } from 'vue'
 import { useEventListener } from '@vueuse/core'
 import { defineStore } from 'pinia'
 
 import { Term } from '../xterm'
-import { useWebShellResourceStore } from './webShellResource'
 import { WebShellResource } from '@/models/resources/webShellResource'
 import { WebShellWSManager } from '@/services/webShell/webShellWSManager'
+import { useWebShellResourceStore } from './webShellResource'
+import { useWebShellSettingStore } from './webShellSettings'
 
-export const useWebshellTermStore = defineStore('webshell-term', () => {
+export const useWebShellTermStore = defineStore('webshell-term', () => {
   const resourceStore = useWebShellResourceStore()
+  const settingStore = useWebShellSettingStore()
 
   const terms = shallowRef<Term[]>([])
   const lastFocusedTerm = shallowRef<Term>()
@@ -27,7 +29,7 @@ export const useWebshellTermStore = defineStore('webshell-term', () => {
   function addTerm(resourceId: string): Promise<void>
   function addTerm(resource: WebShellResource): Promise<void>
   async function addTerm(
-    resourceOrId: string | WebShellResource | undefined = resourceStore.activeManager?.resource,
+    resourceOrId: string | WebShellResource | undefined = resourceStore.selectedResource,
   ) {
     if (!resourceOrId) {
       throw new Error('No resource selected')
@@ -46,6 +48,13 @@ export const useWebshellTermStore = defineStore('webshell-term', () => {
 
     const term = Term.new()
     manager.ptyService.addTerm(term)
+
+    watchEffect(() => {
+      term.options.theme = settingStore.theme
+    })
+    watchEffect(() => {
+      term.options.fontSize = settingStore.fontSize
+    })
 
     terms.value.push(term)
   }
